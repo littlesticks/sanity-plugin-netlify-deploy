c8412660ed381e43a4771601b471bd55662611cf
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-08-25 07:09:30 -0600

    added site name and open netlify link

--- src/NetlifyDeploy.tsx
+++ src/NetlifyDeploy.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import {PublishIcon} from '@sanity/icons'
-import {Flex, Container, Heading, Text, Card, Button as SanityButton} from '@sanity/ui'
+import {Flex, Heading, Text, Card, Button as SanityButton, Grid} from '@sanity/ui'
 
 const getBadgeImage = (siteID: string) => {
   const time = new Date().getTime()
@@ -11,6 +11,7 @@ export default function NetlifyDeploy({config}: {config: any}) {
   const siteID = config.siteID || ''
   const siteURL = config.siteURL || ''
   const buildHookID = config.buildHookID || ''
+  const siteName = config.siteName || ''
 
   const [badgeImageURL, setBadgeImageURL] = useState(
     `https://api.netlify.com/api/v1/badges/${siteID}/deploy-status`
@@ -38,19 +39,30 @@ export default function NetlifyDeploy({config}: {config: any}) {
   }
 
   return (
-    <Container display="grid" width={0} style={{maxWidth: '860px'}}>
+    <Grid autoCols={'fr'} columns={1}>
       <Flex direction="column" padding={4}>
         <Flex align="center" justify="space-between">
           <Card padding={4}>
-            <Heading as="h2">Production</Heading>
-            <Card paddingTop={2}>
+            <Heading as="h1">Production</Heading>
+            <Card paddingTop={4}>
               <img src={badgeImageURL} />
             </Card>
-            <a href={siteURL} target="_blank" rel="noopener noreferrer">
-              <Card paddingTop={4}>
-                <Text>View Site</Text>
-              </Card>
-            </a>
+            <Flex align="center" justify="space-between">
+              <a href={siteURL} target="_blank" rel="noopener noreferrer">
+                <Card paddingTop={4}>
+                  <Text>View Site</Text>
+                </Card>
+              </a>
+              <a
+                href={`https://app.netlify.com/sites/${siteName}/overview`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Card paddingTop={4} paddingLeft={4}>
+                  <Text>Open Netlify</Text>
+                </Card>
+              </a>
+            </Flex>
           </Card>
           <Card padding={4}>
             <SanityButton
@@ -64,6 +76,6 @@ export default function NetlifyDeploy({config}: {config: any}) {
           </Card>
         </Flex>
       </Flex>
-    </Container>
+    </Grid>
   )
 }
